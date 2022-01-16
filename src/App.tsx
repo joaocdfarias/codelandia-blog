aec848366a90b92794993097fdaf0351596d4520
joaocdfarias <joaocdfariass@gmail.com>
2022-01-16 20:49:21 -0300

    feat(core): add search context provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import React from 'react'
 import { BlogSection } from './components/BlogSection'
 
 import { Header } from './components/Header'
+import { SearchContextProvider } from './contexts/SearchContext'
 
 import './style/App.css'
 import './style/Container.css'
 
 function App() {
   return (
-    <div>
-      <Header />
-      <div className="container">
-        <BlogSection />
+    <SearchContextProvider>
+      <div>
+        <Header />
+        <div className="container">
+          <BlogSection />
+        </div>
       </div>
-    </div>
+    </SearchContextProvider>
   )
 }
 
